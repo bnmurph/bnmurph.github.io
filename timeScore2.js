6ca199b72217952d85126b00a1c47c047e053680
ssdrape <146888023+ssdrape@users.noreply.github.com>
2023-11-17 16:49:48 -0500

    TimeScore Runs on nflfastR.csv

--- timeScore2.js
+++ timeScore2.js
@@ -1,63 +1,46 @@
 
 d3.csv("2018-2022_nflfastR_clean.csv").then(
     function (dataset){
-
         console.log(dataset)
-        console.log(d3.map(dataset, d=>d.score_differential_buckets))
-        console.log(d3.group(dataset, d=> d.score_differential_buckets))
-        var score_groups = d3.group(dataset, d=> d.score_differential_buckets)
-        var score_keys = score_groups.keys()
 
-        console.log(score_keys, i => score_groups.get(i))
-        console.log(score_groups.values())
-        for (x of score_groups){
-                var sum = 0
-                for (i of x[1]){
-                        sum += +i.pass
-                }
-                console.log("Pass: "+ x[1].pass + ", " + (+x[1].pass))
-                console.log("Sum: " + sum)
-                console.log(x[1].length)
-                x[1] = sum / x[1].length
-                console.log(x[0] + ", " + x[1])
+        //https://observablehq.com/@d3/d3-group <-- Source for rollup syntax
+        //https://observablehq.com/@d3/d3-flatgroup  <-- Source for flatRollup syntax
+        moose = d3.flatRollup(dataset, i => d3.sum(i, k => k.pass)/i.length, d=>d.score_differential_buckets, d=>d.half_minutes_remaining)
+        console.log(moose)
 
-                
-        }
-        console.log(score_groups)
-/* 
         var dimensions={
             width: 900,
             height: 270,
             margin: {
                 top: 15,
-                bottom: 30,
+                bottom: 50,
                 right: 50,
                 left: 100
             },
             circle_radius: 10
         }
     
-        var score_buckets = i => i.score_differential_buckets
-        var min_remaining = i => i['half_minutes_remaining']
-        var percent_pass = i => i['Pass %']        
+        var score_buckets = i => +i[0]
+        var min_remaining = i => +i[1]
+        var percent_pass = i => i[2]    
 
         var svg = d3.select("#time")
                     .style("width", dimensions.width)
                     .style("height", dimensions.height)
 
-        var xScale = d3.scaleBand()
-                .domain(d3.map(dataset, min_remaining))
-                .range([dimensions.margin.left, dimensions.margin.left + 31*2*dimensions.circle_radius])
+        var xScale = d3.scaleLinear()
+                .domain(d3.extent(moose, min_remaining))
+                .range([dimensions.margin.left + 31*2*dimensions.circle_radius, dimensions.margin.left])
 
-        var yScale = d3.scaleBand()
-                .domain(d3.map(dataset, score_buckets))
-                .range([dimensions.height - dimensions.margin.bottom, dimensions.height - dimensions.margin.bottom - 2*11*dimensions.circle_radius])
+        var yScale = d3.scaleLinear()
+                .domain(d3.extent(moose, score_buckets))
+                .range([dimensions.height - dimensions.margin.bottom, dimensions.height - dimensions.margin.bottom - 2*10*dimensions.circle_radius])
         
         const color = d3.scaleDiverging([0,0.5,1],["blue", "white","red"])
 
         var circles = svg.append("g")
                 .selectAll("circle")
-                .data(dataset)
+                .data(moose)
                 .enter()
                 .append("circle")
                 .on('mouseover', function(){
@@ -78,16 +61,14 @@ d3.csv("2018-2022_nflfastR_clean.csv").then(
                 .attr("fill", d=>color(percent_pass(d)))
 
         var xAxisGen = d3.axisBottom().scale(xScale)
-                .tickValues(xScale.domain().filter(function(d,i){return !(i%2)}))
-
         var xAxis = svg.append("g")
                 .call(xAxisGen)
-                .style("transform", `translate(${-dimensions.circle_radius}px, ${dimensions.height-dimensions.margin.bottom-dimensions.circle_radius}px)`)
+                .style("transform", `translateY(${dimensions.height-dimensions.margin.bottom+dimensions.circle_radius}px)`)
 
         var yAxisGen = d3.axisLeft().scale(yScale)
         var yAxis = svg.append("g")
                 .call(yAxisGen)
-                .style("transform", `translate(${dimensions.margin.left-dimensions.circle_radius}px, ${-dimensions.circle_radius}px)`)
+                .style("transform", `translateX(${dimensions.margin.left-dimensions.circle_radius}px`)
 
         //Axis Title Code from http://www.d3noob.org/2012/12/adding-axis-labels-to-d3js-graph.html
         var xTitle = svg.append("text")
@@ -102,7 +83,7 @@ d3.csv("2018-2022_nflfastR_clean.csv").then(
                 .attr("x", -2*dimensions.height / 3 )
                 //.attr("dy", "1em")
                 .style("text_anchor", "middle")
-                .text("Score Differential")
- */
+                .text("Score Differential") 
+ 
     }
 )
