075c26a6af80a321f3745f64ff5b3080c6778191
ssdrape <146888023+ssdrape@users.noreply.github.com>
2023-11-21 14:08:22 -0500

    Update timeScore2.js

--- timeScore2.js
+++ timeScore2.js
@@ -12,12 +12,12 @@ d3.csv("2018-2022_nflfastR_clean.csv").then(
             width: 900,
             height: 270,
             margin: {
-                top: 15,
-                bottom: 50,
+                top: 5,
+                bottom: 60,
                 right: 50,
                 left: 100
             },
-            circle_radius: 10
+            rect_length: 20
         }
     
         var score_buckets = i => +i[0]
@@ -30,19 +30,19 @@ d3.csv("2018-2022_nflfastR_clean.csv").then(
 
         var xScale = d3.scaleLinear()
                 .domain(d3.extent(moose, min_remaining))
-                .range([dimensions.margin.left + 31*2*dimensions.circle_radius, dimensions.margin.left])
+                .range([dimensions.margin.left + 31*dimensions.rect_length, dimensions.margin.left])
 
         var yScale = d3.scaleLinear()
                 .domain(d3.extent(moose, score_buckets))
-                .range([dimensions.height - dimensions.margin.bottom, dimensions.height - dimensions.margin.bottom - 2*10*dimensions.circle_radius])
+                .range([dimensions.height - dimensions.margin.bottom, dimensions.height - dimensions.margin.bottom - 10*dimensions.rect_length])
         
         const color = d3.scaleDiverging([0,0.5,1],["blue", "white","red"])
 
         var circles = svg.append("g")
-                .selectAll("circle")
+                .selectAll("rect")
                 .data(moose)
                 .enter()
-                .append("circle")
+                .append("rect")
                 .on('mouseover', function(){
                         d3.select(this).style('stroke', 'black')
                                        .style('stroke-width', 1)
@@ -55,20 +55,21 @@ d3.csv("2018-2022_nflfastR_clean.csv").then(
                         //filter if not filtered
                         //unfilter if filtered
                 })
-                .attr("cx", d => xScale(min_remaining(d)))
-                .attr("cy", d => yScale(score_buckets(d)))
-                .attr("r", dimensions.circle_radius)
+                .attr("x", d => xScale(min_remaining(d)))
+                .attr("y", d => yScale(score_buckets(d)))
+                .attr("width", dimensions.rect_length)
+                .attr("height", dimensions.rect_length)
                 .attr("fill", d=>color(percent_pass(d)))
 
         var xAxisGen = d3.axisBottom().scale(xScale)
         var xAxis = svg.append("g")
                 .call(xAxisGen)
-                .style("transform", `translateY(${dimensions.height-dimensions.margin.bottom+dimensions.circle_radius}px)`)
+                .style("transform", `translate(${dimensions.rect_length/2}px,${dimensions.height-dimensions.margin.bottom+dimensions.rect_length}px)`)
 
-        var yAxisGen = d3.axisLeft().scale(yScale)
+        var yAxisGen = d3.axisLeft().scale(yScale).tickValues(d3.map(moose, score_buckets))
         var yAxis = svg.append("g")
                 .call(yAxisGen)
-                .style("transform", `translateX(${dimensions.margin.left-dimensions.circle_radius}px`)
+                .style("transform", `translate(${dimensions.margin.left}px, ${dimensions.rect_length/2}px)`)
 
         //Axis Title Code from http://www.d3noob.org/2012/12/adding-axis-labels-to-d3js-graph.html
         var xTitle = svg.append("text")
